5fe1f410b13f72f0cb4ecb284ebf5ec759411c8e
G3R41D <gcaunah406@gmail.com>
2025-10-06 08:36:52 -0500

    modelo, controlador y rutas

---
+++ app/Models/Note.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    /** @use HasFactory<\Database\Factories\NoteFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'deadline',
+        'done'
+    ];
+}
---
+++ database/factories/NoteFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Note>
+ */
+class NoteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(3),
+            'description' => $this->faker->paragraph(3),
+            'deadline' => $this->faker->dateTimeBetween('now', '+1 year'),
+            'done'=> $this->faker->boolean(20)
+        ];
+    }
+}
